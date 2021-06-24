745ae5e6530d656e227f9faee3aaab07cc7110ba
phongice <vnkye2@gmail.com>
2021-06-24 15:30:19 +0700

    aaa

--- app/src/main/java/com/example/exercises7_monstarlabs/helper/CommunicationAdapterAudio.kt
+++
@@ -1,7 +0,0 @@
-package com.example.exercises7_monstarlabs.helper
-
-import com.example.exercises7_monstarlabs.model.MyAudio
-
-interface CommunicationAdapterAudio {
-    fun setOnClickItem(position:Int)
-}
